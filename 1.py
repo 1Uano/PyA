04a7aff46125b0707263edc1a0957a34db532366
godofthepython <ivan2005makarenko@gmail.com>
2019-09-14 16:37:09 +0300

    2

--- 1.py
+++ 1.py
@@ -2,11 +2,15 @@
 
 class Critter(object):
     """Виртуальный питомец"""
+    def __init__(self):
+        print("Появилась зверюшка")
+
     def talk(self):
-        print("Привет я твоя зверюшка")
+        print("Привет я твой зверюшка")
 
 def main():
-    crit = Critter()
-    crit.talk()
-
+    crit1 = Critter()
+    crit1.talk()
+    crit2 = Critter()
+    crit2.talk()
 main()
