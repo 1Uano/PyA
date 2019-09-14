1de81a2f520cd2c559f15cef39e2a0af0f073ce9
godofthepython <ivan2005makarenko@gmail.com>
2019-09-14 18:25:12 +0300

    8

--- 1.py
+++ 1.py
@@ -17,6 +17,10 @@ class Critter(object):
         ans += 'имя:' + self.name 
         return ans
 
+    def __pass_time(self):
+        self.hunger +=1
+        self.boredom +=1
+
     @property
     def name(self):
         return self.__name
@@ -28,24 +32,51 @@ class Critter(object):
         else:
             self.__name = new_name
             print('Имя успешно не изменено.')
+    
+    @property
+    def mood(self):
+        unhappiness = self.hunger + self.boredom
+        if unhappiness < 5:
+            m = 'Не ну ты бог'
+        elif 5 <= unhappiness <= 100:
+            m = 'сойдет'
+        else:
+            m='Я звоню в ментуру'
+        return m
 
     def talk(self):
         print("Меня зовут" ,  self.name)
+        self.__pass_time()
+
+    def eat(self , food = 4):
+        print("Мрр.....спасибо")
+        self.hunger -= food
+        if self.hunger < 0:
+            self.hunger = 0
+        self.__pass_time()
+
+    def play(self , fun = 4):
+        print("Хрю")
+        self.boredom -= fun
+        if self.boredom < 0:
+            self.boredom = 0
+        self.__pass_time()
 
 def main():
     print('Создание зверюшек')
-    crit1 = Critter("Зверюшка1" , 100000000 , -1000000000)
+    crit1 = Critter("Зверюшка1" , 7 , 7)
     crit2 = Critter("Зверюшка2")
 
     Critter.status()
 
-    crit1.name ="Бобик"
-    print('Имя зверушки:' , crit1.name)
-    
-    crit2.name =""
-    print('Имя зверушки:' , crit2.name)
+    crit1.talk()
+    crit1.eat()
+    crit1.play()
+    print(crit1.mood)
 
+    
     print(crit1)
     print('Голод: ' + str(crit1.hunger))
-    print('Настроение: ' + str(crit1.boredom))
+    print('Усталость: ' + str(crit1.boredom) , '\n')
+    
 main()
